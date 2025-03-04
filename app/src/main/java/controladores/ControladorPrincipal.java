727e457c46101999ceda698ca7d5f6bf467ef647
Pablo López Lozano <plopezlozano12@gmail.com>
2025-03-04 18:21:56 +0100

    Vista Principal

--- app/src/main/java/controladores/ControladorPrincipal.java
+++ app/src/main/java/controladores/ControladorPrincipal.java
@@ -4,10 +4,43 @@
  */
 package controladores;
 
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import modelo.Carrera;
+
 /**
  *
  * @author allae
  */
-public class ControladorPrincipal {
+public class ControladorPrincipal implements Initializable{
+    
+    public static ObservableList<Carrera> getCarrerasList() {
+        return FXCollections.observableArrayList(
+                Arrays.asList(
+                        new Carrera("Maratón de Granada", "Carrera anual en Granada", new Date(2025-1900, 4, 10), 42.195, "Granada, España", "37.1773, -3.5986", 30.00, 5000, "Abierta", "Maratón", ""),
+                        new Carrera("Media Maratón de Sevilla", "Competencia de media maratón", new Date(2025-1900, 4, 10), 21.097, "Sevilla, España", "37.3886, -5.9823", 25.00, 4000, "Abierta", "Media Maratón", ""),
+                        new Carrera("Carrera 10K Madrid", "Carrera urbana de 10K", new Date(2025-1900, 4, 10), 10.0, "Madrid, España", "40.4168, -3.7038", 20.00, 3000, "Cerrada", "10K", ""),
+                        new Carrera("Ultra Trail Pirineos", "Carrera de montaña extrema", new Date(2025-1900, 4, 10), 100.0, "Pirineos, España", "42.6675, 0.5863", 50.00, 1000, "Abierta", "Ultra Trail", "")
+                )
+        );
+    }
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<Carrera> carreras = getCarrerasList();
+       
+        System.out.println(carreras.toString());
+ 
+    }
     
 }
