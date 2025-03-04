60af64d76713104c29aa55e9f31bfe076260d060
Axlxe05 <158088989+Axlxe05@users.noreply.github.com>
2025-03-04 17:05:30 +0100

    create JAVAFX desktop

---
+++ Runnify-DAMDAW/app/src/main/java/Runnify/DAMDAW/App.java
@@ -0,0 +1,33 @@
+package Runnify.DAMDAW;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        
+       
+       FXMLLoader hola = new FXMLLoader(getClass().getResource("/vista/VistaPrincipal.fxml"));
+       Parent adios = hola.load();
+       
+
+       Scene escena = new Scene(adios);
+       primaryStage.setScene(escena);
+       primaryStage.setTitle("Proyecto Runnify");
+       primaryStage.show();
+       
+        
+    }
+   
+}
