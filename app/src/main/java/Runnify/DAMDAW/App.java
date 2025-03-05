fe8621c62538f374bb11311f300d91237f541477
gonzaloangulo <gonzalounion17@gmail.com>
2025-03-05 16:57:34 +0100

    LogoApp

--- app/src/main/java/Runnify/DAMDAW/App.java
+++ app/src/main/java/Runnify/DAMDAW/App.java
@@ -1,33 +1,31 @@
 package Runnify.DAMDAW;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    
-    
+
     public static void main(String[] args) {
         launch(args);
     }
     
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader hola = new FXMLLoader(getClass().getResource("/vista/VistaLogin.fxml"));
+        Parent adios = hola.load();
+        
+        Scene escena = new Scene(adios);
+        primaryStage.setScene(escena);
+        primaryStage.setTitle("Proyecto Runnify");
         
        
-       FXMLLoader hola = new FXMLLoader(getClass().getResource("/vista/VistaLogin.fxml"));
-       Parent adios = hola.load();
-       
-
-       Scene escena = new Scene(adios);
-       primaryStage.setScene(escena);
-       primaryStage.setTitle("Proyecto Runnify");
-       primaryStage.show();
-       
+        Image logo = new Image(getClass().getResource("/img/LOGO.png").toExternalForm());
+        primaryStage.getIcons().add(logo);
         
+        primaryStage.show();
     }
-   
 }
