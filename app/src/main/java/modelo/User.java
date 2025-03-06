a15fd9e0ab4807a95a75e7147402e5ec8fa7afd4
Pablo López Lozano <plopezlozano12@gmail.com>
2025-03-06 20:17:58 +0100

    Update User.java

--- app/src/main/java/modelo/User.java
+++ app/src/main/java/modelo/User.java
@@ -4,10 +4,67 @@
  */
 package modelo;
 
+import java.util.List;
+
 /**
  *
  * @author plope
  */
 public class User {
+    private int id;
+    private String email;
+    private List<String> roles;
+    private String name;
+    private boolean banned;
+
+    public User(int id, String email, List<String> roles, String name, boolean banned) {
+        this.id = id;
+        this.email = email;
+        this.roles = roles;
+        this.name = name;
+        this.banned = banned;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isBanned() {
+        return banned;
+    }
+
+    public void setBanned(boolean banned) {
+        this.banned = banned;
+    }
+    
+    
     
 }
