ddc97caca6a6f580c89e7d7d5bed238127756d86
Axlxe05 <158088989+Axlxe05@users.noreply.github.com>
2025-03-07 03:20:44 +0100

    Create MiPerfilComponente.java

---
+++ app/src/main/java/componentevisual/MiPerfilComponente.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package componentevisual;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import modelo.Carrera;
+
+/**
+ *
+ * @author allae
+ */
+public class MiPerfilComponente extends VBox {
+
+    private final ImageView imgPerfil;
+    private final TextField txtNombre;
+    private final TextField txtEmail;
+    private final TextField txtSexo;
+    private final TextField txtEdad;
+    private final TextField txtContraseña;
+    private final Button btnEditar;
+    private final Button btnAceptar;
+    
+
+    public MiPerfilComponente() {
+        super();
+
+
+    }
+
+}
